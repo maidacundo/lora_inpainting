2482d2e1e460b1bcf78e6f96bd1b46bcc99dfb21
Facundo Maidana <maidacundo@gmail.com>
2023-09-17 11:28:36 +0200

    normalization fix

--- lora.py
+++ lora.py
@@ -36,11 +36,11 @@ class LoraInjectedLinear(nn.Module):
         nn.init.normal_(self.lora_down.weight, std=1 / r)
         nn.init.zeros_(self.lora_up.weight)
 
-    def forward(self, input):
+    def forward(self, input, scale):
         return (
             self.linear(input)
             + self.dropout(self.lora_up(self.selector(self.lora_down(input))))
-            * self.scale
+            * scale
         )
 
     def realize_as_lora(self):
