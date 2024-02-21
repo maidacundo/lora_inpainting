09ca54a89c1aac33a647385b69090d8bfaa570a9
Facundo Maidana <facundo.maidana@studio.unibo.it>
2024-02-21 21:34:09 +0100

    timesteps

--- training_scripts/experiments/run_training.sh
+++ training_scripts/experiments/run_training.sh
@@ -31,3 +31,20 @@ python3 test_different_base_model.py --dataset kvist_windows --base-model realis
 python3 test_different_base_model.py --dataset sommerhus --base-model realistic
 python3 test_different_base_model.py --dataset 7er_stol --base-model realistic
 python3 test_different_base_model.py --dataset dinesen --base-model realistic
+
+# TIMESTEPS TESTING
+
+python3 test_timesteps.py --dataset kvist_windows --upper-bound 1000 --lower-bound 800
+python3 test_timesteps.py --dataset sommerhus --upper-bound 1000 --lower-bound 800
+
+python3 test_timesteps.py --dataset kvist_windows --upper-bound 800 --lower-bound 600
+python3 test_timesteps.py --dataset sommerhus --upper-bound 800 --lower-bound 600
+
+python3 test_timesteps.py --dataset kvist_windows --upper-bound 600 --lower-bound 400
+python3 test_timesteps.py --dataset sommerhus --upper-bound 600 --lower-bound 400
+
+python3 test_timesteps.py --dataset kvist_windows --upper-bound 400 --lower-bound 200
+python3 test_timesteps.py --dataset sommerhus --upper-bound 400 --lower-bound 200
+
+python3 test_timesteps.py --dataset kvist_windows --upper-bound 200 --lower-bound 1
+python3 test_timesteps.py --dataset sommerhus --upper-bound 200 --lower-bound 1
